f90234e161594e737dfad6467d47e68699cf497e
jeffrey-ross <rossjeffreyvs-dev@users.noreply.github.com>
2026-04-27 19:18:11 -0400

    customer lifecycle nav bar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -127,7 +127,7 @@ export default function Navbar() {
 
                     <li>
                       <Link
-                        href="https://clinical-trial-patient-agent.jeffrey-ross.me"
+                        href="https://jr-portfolio-projects.dtw628ha8cm94.us-west-2.cs.amazonlightsail.com/agentic-customer-lifecycle-platform"
                         className="flex items-start space-x-2 hover:text-black dark:hover:text-white transition"
                       >
                         <span className="mr-2">🧬</span>
